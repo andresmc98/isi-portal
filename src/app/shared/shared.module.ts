23af9d6092e9cc108b646e8ee12d43e43bbeffdf
Luis Romo Portillo <luisrp9812@gmail.com>
2023-10-16 23:16:39 -0700

    refactory of sidenav with angular material

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,24 +4,11 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { AppMaterialModule } from './app-material/app-material.module';
 import { RouterModule } from '@angular/router';
-
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [
-    FooterComponent,
-    HeaderComponent
-  ],
-  imports: [
-    CommonModule,
-    AppMaterialModule,
-    RouterModule
-  ],
-  exports: [
-    AppMaterialModule,
-    CommonModule,
-    FooterComponent,
-    HeaderComponent
-  ]
+  declarations: [FooterComponent, HeaderComponent],
+  imports: [CommonModule, AppMaterialModule, RouterModule, MatToolbarModule],
+  exports: [AppMaterialModule, CommonModule, FooterComponent, HeaderComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
