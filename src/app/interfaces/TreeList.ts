bc97f0cf5a8abebe0b25447780d91c39f3674d38
Luis Romo <72539524+luisrp98@users.noreply.github.com>
2024-01-17 00:54:28 -0700

    add treeList default component

---
+++ src/app/interfaces/TreeList.ts
@@ -0,0 +1,29 @@
+export interface DefaultNode {
+  name: string;
+  children?: DefaultNode[];
+}
+
+export interface DefaultFlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+}
+export const TREE_DATA: DefaultNode[] = [
+  {
+    name: 'Fruit',
+    children: [{ name: 'Apple' }, { name: 'Banana' }, { name: 'Fruit loops' }],
+  },
+  {
+    name: 'Vegetables',
+    children: [
+      {
+        name: 'Green',
+        children: [{ name: 'Broccoli' }, { name: 'Brussels sprouts' }],
+      },
+      {
+        name: 'Orange',
+        children: [{ name: 'Pumpkins' }, { name: 'Carrots' }],
+      },
+    ],
+  },
+];
