5c6d0c17636238c303989030d73f967e7e985374
Luis Romo <72539524+luisrp98@users.noreply.github.com>
2024-02-21 00:47:17 -0700

    refactor: club-menu to tailwind

--- src/app/components/club-menu/club-menu.component.ts
+++ src/app/components/club-menu/club-menu.component.ts
@@ -6,28 +6,28 @@ import { environment } from 'src/environment/environment';
 @Component({
   selector: 'app-club-menu',
   templateUrl: './club-menu.component.html',
-  styleUrls: ['./club-menu.component.scss'],
 })
-export class ClubMenuComponent implements OnInit{
-
-  clubes : Doc[] = [];
+export class ClubMenuComponent implements OnInit {
+  clubes: Doc[] = [];
   //apiUrl = environment.localMediaUrl; //local
   apiUrl = environment.baseMediaUrl; //server
 
-  constructor(private apiservice: ApiService) { }
+  constructor(private apiservice: ApiService) {}
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.getClubes();
-
   }
 
-  getClubes(){
+  getClubes() {
     console.log('getClubesPayload');
-    this.apiservice.getClubes().subscribe(res=>{
-      console.log('getClubesResponse', res);
-      this.clubes = res.docs;
-    },error =>{
-      console.log('getClubesError', error);
-    })
+    this.apiservice.getClubes().subscribe(
+      (res) => {
+        console.log('getClubesResponse', res);
+        this.clubes = res.docs;
+      },
+      (error) => {
+        console.log('getClubesError', error);
+      }
+    );
   }
 }
