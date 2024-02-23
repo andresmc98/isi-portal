8d9e6b655c1ce92e2071d213060cd86bd268ffe3
andresmc98 <jossean98@gmail.com>
2024-02-23 14:50:55 -0700

    Chore: refactor to tailwind about us

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    screens: {
+      'tablet': "640px",
+      // => @media (min-width: 640px)
+      'laptop': "1024px",
+      // => @media (min-width: 1024px)
+      'desktop': "1280px",
+      // => @media (min-width: 1280px)
+    },
     extend: {
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
       fontFamily: {
         lexend: ["Lexend", "sans"],
       },
@@ -16,5 +31,16 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
 };
