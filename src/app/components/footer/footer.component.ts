bfefeb85ed4772bde84f6094678741f552f3dbc1
Luis Romo <72539524+luisrp98@users.noreply.github.com>
2024-02-24 00:05:56 -0700

    refactor: footer component to tailwind

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,8 +3,45 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  info = [
+    {
+      enlace: 'https://maps.app.goo.gl/zq7ETmXosQ7tJ6Nb7',
+      icono: 'location_on',
+      info: 'Avenida Universidad con Calle de la Sabiduría, Edificio 5J, Planta Baja. Hermosillo, Son.',
+    },
+    {
+      enlace: 'mailto:departamentoisi@unison.mx',
+      icono: 'email',
+      info: 'departamentoisi@unison.mx',
+    },
+    {
+      enlace: 'tel:662-992-20-94',
+      icono: 'call',
+      info: '662-992-20-94',
+    },
+  ];
+  social = [
+    {
+      enlace: 'https://www.facebook.com/profile.php?id=100002092122881',
+      icono: 'facebook',
+      hoverColor: 'hover:text-blue-700',
+    },
+    {
+      enlace: 'https://twitter.com/ISIUNISON',
+      icono: 'x',
+      hoverColor: 'hover:text-black/30',
+    },
+    {
+      enlace: 'https://www.instagram.com/',
+      icono: 'instagram',
+      hoverColor: 'hover:text-pink-600',
+    },
+    {
+      enlace: 'https://www.youtube.com/',
+      icono: 'youtube',
+      hoverColor: 'hover:text-red-600',
+    },
+  ];
 }
