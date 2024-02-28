0a3edaf624c73e5e76461c402a470b1b06ed8b33
andresmc98 <jossean98@gmail.com>
2024-02-28 14:43:48 -0700

    fix: media lost

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -21,7 +21,7 @@ export class AboutUsComponent implements OnInit{
   ngOnInit(): void{
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.getMediaByID('65d8d1e0a859f8678c2d31ac')
+    this.getMediaByID('65dfa8d92fdc50ead6b582d4')
     console.log('MediaUs', this.mediaUs);
     this.getDevs();
   }
