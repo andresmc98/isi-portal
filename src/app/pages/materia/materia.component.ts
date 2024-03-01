d243a3caf05608e89bc79360f8fa1fea5d943582
Luis <a222212359@unison.mx>
2024-02-29 23:06:27 -0700

    ✨ Feat: Add v1 of materia component

--- src/app/pages/materia/materia.component.ts
+++ src/app/pages/materia/materia.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-materia',
   templateUrl: './materia.component.html',
-  styleUrls: ['./materia.component.scss']
 })
 export class MateriaComponent {
 
