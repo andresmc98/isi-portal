7e313d36c3d3e0f01b8522e0705ec12d4e10c038
Luis Romo Portillo <luisrp9812@gmail.com>
2024-03-12 16:43:59 -0700

    ✨ Feat: Added styles mobile for noticias component

--- src/app/pages/noticias/noticias.component.ts
+++ src/app/pages/noticias/noticias.component.ts
@@ -1,32 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
-import { Observable } from 'rxjs';
-import { Contenido, Noticias } from 'src/app/interfaces/Noticias';
+import { Noticias, Noticia } from 'src/app/interfaces/Noticias';
+import { environment } from 'src/environment/environment';
 
 @Component({
   selector: 'app-noticias',
   templateUrl: './noticias.component.html',
-  styleUrls: ['./noticias.component.scss'],
 })
-export class NoticiasComponent {
-  // noticias: Doc[] = [];
-  // constructor(
-  //   private apiService: ApiService
-  // ) {
-  // };
-  // ngOnInit(): void {
-  //   //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-  //   //Add 'implements OnInit' to the class.
-  //   this.getNoticias();
-  // }
-  // getNoticias() {
-  //   this.apiService.getNoticias().subscribe({
-  //     next: (data: Noticias) => {
-  //       this.noticias = data.docs;
-  //     },
-  //     error:(err) =>{
-  //       this.noticias = [];
-  //     },
-  //   });
-  // }
+export class NoticiasComponent implements OnInit {
+  noticias: Noticia[] = [];
+  apiUrl = environment.baseMediaUrl;
+
+  constructor(private apiservice: ApiService) {}
+
+  ngOnInit() {
+    this.apiservice.getNoticias().subscribe((data) => {
+      this.noticias = data.docs;
+      console.log(this.noticias);
+    });
+  }
 }
