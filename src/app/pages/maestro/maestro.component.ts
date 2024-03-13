561c1f72e80afa071b865cd10d1c28acecd25f56
Luis Romo Portillo <luisrp9812@gmail.com>
2024-03-12 19:59:05 -0700

    ✨ Feat: Added mobile styles for maestro component

--- src/app/pages/maestro/maestro.component.ts
+++ src/app/pages/maestro/maestro.component.ts
@@ -7,10 +7,9 @@ import { environment } from 'src/environment/environment';
 @Component({
   selector: 'app-maestro',
   templateUrl: './maestro.component.html',
-  styleUrls: ['./maestro.component.scss'],
 })
 export class MaestroComponent {
-  maestro!: Maestro;
+  maestro: Maestro = {} as Maestro;
   apiUrl = environment.baseMediaUrl;
 
   constructor(private apiservice: ApiService, private route: ActivatedRoute) {}
